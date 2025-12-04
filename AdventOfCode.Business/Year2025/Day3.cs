2fc366abdf6a9e22a59d96053196ca22f00028bb
Elliot Moule <ellimyt@live.com.au>
2025-12-04 17:13:57 +1100

    [Stubbing Day3 - Part 1]

---
+++ AdventOfCode.Business/Year2025/Day3.cs
@@ -0,0 +1,23 @@
+﻿namespace AdventOfCode.Business.Year2025
+{
+    internal class Day3 : IAdventDay
+    {
+        public void ExecutePart1()
+        {
+            var day3Input = File.ReadAllText(Path.Combine("Year2025", "Resources", "Day3_Input.txt"));
+            var result = SumOfProducedJoltage(day3Input);
+
+            Console.WriteLine($"\r\nThe dial was at position 0 a total of {result} times.\r\n");
+        }
+
+        public void ExecutePart2()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static uint SumOfProducedJoltage(string input)
+        {
+            return 0;
+        }
+    }
+}
---
+++ AdventOfCode.Test/Year2025/Day3Tests.cs
@@ -0,0 +1,25 @@
+﻿namespace AdventOfCode.Test.Year2025
+{
+    internal class Day3Tests
+    {
+        [Test]
+        public void SumOfProducedJoltage_WhenInputIsProvided_ShouldReturnExpectedResult()
+        {
+            // Arrange
+            var input =
+                """
+                987654321111111
+                811111111111119
+                234234234234278
+                818181911112111
+                """;
+
+            // Act
+            var result = Business.Year2025.Day3.SumOfProducedJoltage(input);
+
+            // Assert
+            uint expected = 357;
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
