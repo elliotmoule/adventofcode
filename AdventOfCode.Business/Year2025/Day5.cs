1d5fc1f06df5df22457d0bbca346611dd6229822
Elliot Moule <ellimyt@live.com.au>
2025-12-05 17:10:37 +1100

    [Day 5 - Part 1] - Stubbing out methods and tests.

---
+++ AdventOfCode.Business/Year2025/Day5.cs
@@ -0,0 +1,28 @@
+﻿namespace AdventOfCode.Business.Year2025
+{
+    internal class Day5 : IAdventDay
+    {
+        public void ExecutePart1()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ExecutePart2()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal Dictionary<uint, Ingredient> BuildIngredientDatabase(string database)
+        {
+            return [];
+        }
+
+        internal record Ingredient(IngredientState State, bool Overlaps);
+
+        internal enum IngredientState
+        {
+            Fresh,
+            Spoiled,
+        }
+    }
+}
---
+++ AdventOfCode.Test/Year2025/Day5Tests.cs
@@ -0,0 +1,24 @@
+﻿using AdventOfCode.Business.Year2025;
+using static AdventOfCode.Business.Year2025.Day5;
+
+namespace AdventOfCode.Test.Year2025
+{
+    internal class Day5Tests
+    {
+        [Test]
+        public void BuildIngredientDatabase_ShouldReturnCorrectDictionary()
+        {
+            // Arrange
+            var day5 = new Day5();
+            var rawDatabase = File.ReadAllText(Path.Combine("Year2025", "Resources", "Example", "Day5_Input.txt"));
+
+            // Act
+            var result = day5.BuildIngredientDatabase(rawDatabase);
+
+            // Assert
+            Assert.That(result, Has.Count.EqualTo(5));
+            Assert.That(result.LastOrDefault().Value, Is.Not.Null);
+            Assert.That(result.LastOrDefault().Value.State, Is.EqualTo(IngredientState.Spoiled));
+        }
+    }
+}
