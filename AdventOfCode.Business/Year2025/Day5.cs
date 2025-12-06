55f4be95a36827b7a47875d390cb65911f070dd0
Elliot Moule <ellimyt@live.com.au>
2025-12-06 15:16:10 +1100

    [Day 5 - Part 1 Complete]

--- AdventOfCode.Business/Year2025/Day5.cs
+++ AdventOfCode.Business/Year2025/Day5.cs
@@ -4,7 +4,10 @@
     {
         public void ExecutePart1()
         {
-            throw new NotImplementedException();
+            var day5Input = File.ReadAllLines(Path.Combine("Year2025", "Resources", "Actual", "Day5_Input.txt"));
+            var result = CalculateHowManyIngredientsAreFresh(day5Input);
+
+            Console.WriteLine($"\r\nThere are {result} fresh ingredient IDs.\r\n");
         }
 
         public void ExecutePart2()
@@ -12,12 +15,132 @@
             throw new NotImplementedException();
         }
 
-        internal Dictionary<uint, Ingredient> BuildIngredientDatabase(string database)
+        internal static uint CalculateHowManyIngredientsAreFresh(string[] input)
         {
-            return [];
+            ArgumentNullException.ThrowIfNull(input);
+
+            if (input.Length == 0)
+            {
+                return 0u;
+            }
+
+            var (ingredientIds, freshIngredientIds) = BuildIngredientLists(input);
+
+            Dictionary<ushort, Ingredient> ingredientsDatabase = BuildFreshIngredientDatabase(freshIngredientIds);
+
+            var freshCount = GetFreshIngredientCountFromAvailableIngredientIds(ingredientIds, ingredientsDatabase);
+
+            return freshCount;
         }
 
-        internal record Ingredient(IngredientState State, bool Overlaps);
+        internal static (List<ushort> ingredientIds, List<ushort> freshIngredientIds) BuildIngredientLists(string[] input)
+        {
+            ArgumentNullException.ThrowIfNull(input);
+
+            if (input.Length == 0)
+            {
+                throw new ArgumentException("Must contain input to build ingredient lists.");
+            }
+
+            var isRangeSection = true;
+            List<ushort> ingredientIds = [];
+            List<ushort> freshIngredientIds = [];
+
+            foreach (var line in input)
+            {
+                if (isRangeSection)
+                {
+                    if (string.IsNullOrWhiteSpace(line))
+                    {
+                        isRangeSection = false;
+                        continue;
+                    }
+                    var range = GetRange(line);
+                    for (ushort i = range.Start; i <= range.End; i++)
+                    {
+                        freshIngredientIds.Add(i);
+                    }
+                }
+                else
+                {
+                    if (string.IsNullOrWhiteSpace(line))
+                    {
+                        continue;
+                    }
+                    var ingredientId = ushort.Parse(line);
+                    ingredientIds.Add(ingredientId);
+                }
+            }
+            return (ingredientIds, freshIngredientIds);
+        }
+
+        internal static URange GetRange(string line)
+        {
+            if (string.IsNullOrWhiteSpace(line))
+            {
+                throw new ArgumentException("Line cannot be null or whitespace", nameof(line));
+            }
+
+            var parts = line.Split('-', StringSplitOptions.RemoveEmptyEntries);
+            if (parts.Length != 2)
+            {
+                throw new FormatException("Line is not in the correct format");
+            }
+            var start = ushort.Parse(parts[0]);
+            var end = ushort.Parse(parts[1]);
+
+            return new URange(start, end);
+        }
+
+        internal static Dictionary<ushort, Ingredient> BuildFreshIngredientDatabase(List<ushort> freshIngredientIds)
+        {
+            ArgumentNullException.ThrowIfNull(freshIngredientIds, nameof(freshIngredientIds));
+
+            if (freshIngredientIds.Count == 0)
+            {
+                return [];
+            }
+
+            Dictionary<ushort, Ingredient> ingredientsDatabase = [];
+            foreach (var fresh in freshIngredientIds.OrderBy(x => x))
+            {
+                if (ingredientsDatabase.ContainsKey(fresh))
+                {
+                    ingredientsDatabase[fresh] = new Ingredient(fresh, IngredientState.Fresh, true);
+                    continue;
+                }
+
+                ingredientsDatabase[fresh] = new Ingredient(fresh, IngredientState.Fresh, false);
+            }
+
+            return ingredientsDatabase;
+        }
+
+        internal static uint GetFreshIngredientCountFromAvailableIngredientIds(List<ushort> ingredientIds, Dictionary<ushort, Ingredient> ingredientsDatabase)
+        {
+            ArgumentNullException.ThrowIfNull(ingredientIds);
+            ArgumentNullException.ThrowIfNull(ingredientsDatabase);
+
+            if (ingredientIds.Count == 0 || ingredientsDatabase.Count == 0)
+            {
+                return 0;
+            }
+
+            var freshCount = 0u;
+            foreach (var ingredientId in ingredientIds.OrderBy(x => x))
+            {
+                if (ingredientsDatabase.ContainsKey(ingredientId))
+                {
+                    freshCount++;
+                }
+            }
+
+            return freshCount;
+        }
+
+        internal record URange(ushort Start, ushort End);
+
+        internal record Ingredient(uint Id, IngredientState IngredientState, bool? Overlaps);
 
         internal enum IngredientState
         {
--- AdventOfCode.Test/Year2025/Day5Tests.cs
+++ AdventOfCode.Test/Year2025/Day5Tests.cs
@@ -6,19 +6,274 @@ namespace AdventOfCode.Test.Year2025
     internal class Day5Tests
     {
         [Test]
-        public void BuildIngredientDatabase_ShouldReturnCorrectDictionary()
+        public void CalculateHowManyIngredientsAreFresh_NullInput_ThrowsArgumentNullException()
         {
-            // Arrange
-            var day5 = new Day5();
-            var rawDatabase = File.ReadAllText(Path.Combine("Year2025", "Resources", "Example", "Day5_Input.txt"));
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() => { CalculateHowManyIngredientsAreFresh(null!); });
+        }
 
+        [Test]
+        public void CalculateHowManyIngredientsAreFresh_EmptyInput_ReturnsZero()
+        {
             // Act
-            var result = day5.BuildIngredientDatabase(rawDatabase);
+            var result = CalculateHowManyIngredientsAreFresh([]);
 
             // Assert
-            Assert.That(result, Has.Count.EqualTo(5));
-            Assert.That(result.LastOrDefault().Value, Is.Not.Null);
-            Assert.That(result.LastOrDefault().Value.State, Is.EqualTo(IngredientState.Spoiled));
+            Assert.That(result, Is.EqualTo(0u));
+        }
+
+        [Test]
+        public void CalculateHowManyIngredientsAreFresh_ValidInput_ReturnsCorrectResult()
+        {
+            // Arrange
+            var inputLines = File.ReadAllLines(Path.Combine("Year2025", "Resources", "Example", "Day5_Input.txt"));
+
+            // Act
+            var result = Day5.CalculateHowManyIngredientsAreFresh(inputLines);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(3u));
+        }
+
+        [Test]
+        public void BuildIngredientLists_NullInput_ThrowsArgumentNullException()
+        {
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() => BuildIngredientLists(null!));
+        }
+
+        [Test]
+        public void BuildIngredientLists_EmptyInput_ThrowsArgumentNullException()
+        {
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => BuildIngredientLists([]));
+        }
+
+        [Test]
+        public void BuildIngredientLists_ValidInput_ReturnsCorrectResult()
+        {
+            // Arrange
+            List<ushort> expectedIngredientIds = [1, 2, 8, 12, 15, 19];
+            List<ushort> expectedFreshIngredientIds = [2, 3, 4, 5, 6, 7, 8, 6, 7, 8, 9, 10, 11, 12, 14, 15, 16];
+            (List<ushort> ingredientIds, List<ushort> freshIngredientIds) expected = new(expectedIngredientIds, expectedFreshIngredientIds);
+            string[] input =
+                [
+                    "2-8",
+                    "6-12",
+                    "14-16",
+                    "",
+                    "1",
+                    "2",
+                    "8",
+                    "12",
+                    "15",
+                    "19",
+                ];
+
+            // Act
+            var (ingredientIds, freshIngredientIds) = BuildIngredientLists(input);
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(ingredientIds, Is.EquivalentTo(expected.ingredientIds));
+                Assert.That(freshIngredientIds, Is.EquivalentTo(expected.freshIngredientIds));
+            });
+        }
+
+        [Test]
+        public void GetRange_MissingRangeDividerInput_ThrowsFormatException()
+        {
+            // Arrange
+            var input = "14 17";
+
+            // Act & Assert
+            Assert.Throws<FormatException>(() => GetRange(input));
+        }
+
+        [Test]
+        public void GetRange_NonNumericInputForMin_ThrowsFormatException()
+        {
+            // Arrange
+            var input = "Z 17";
+
+            // Act & Assert
+            Assert.Throws<FormatException>(() => GetRange(input));
+        }
+
+        [Test]
+        public void GetRange_NonNumericInputForMax_ThrowsFormatException()
+        {
+            // Arrange
+            var input = "8 Z";
+
+            // Act & Assert
+            Assert.Throws<FormatException>(() => GetRange(input));
+        }
+
+        [Test]
+        public void GetRange_ValidInput_ReturnsCorrectResult()
+        {
+            // Arrange
+            var expected = new URange(12, 26);
+            var input = "12-26";
+
+            // Act
+            var result = GetRange(input);
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(result.Start, Is.EqualTo(12u));
+                Assert.That(result.End, Is.EqualTo(26u));
+                Assert.That(result, Is.EqualTo(expected));
+            });
+        }
+
+        [Test]
+        public void BuildFreshIngredientDatabase_NullInput_ThrowsArgumentNullException()
+        {
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() => BuildFreshIngredientDatabase(null!));
+        }
+
+        [Test]
+        public void BuildFreshIngredientDatabase_EmptyInput_ReturnsEmpty()
+        {
+            // Act
+            var result = BuildFreshIngredientDatabase([]);
+
+            // Assert
+            Assert.That(result, Is.Empty);
+        }
+
+        [Test]
+        public void BuildFreshIngredientDatabase_WithMixedNumbers_ReturnsOrderedDictionary()
+        {
+            // Arrange
+            var expected = new Dictionary<ushort, Ingredient>
+            {
+                { 2, new(2, IngredientState.Fresh, false) },
+                { 3, new(3, IngredientState.Fresh, false) },
+                { 5, new(5, IngredientState.Fresh, false) },
+                { 6, new(6, IngredientState.Fresh, false) },
+                { 7, new(7, IngredientState.Fresh, false) },
+                { 12, new(12, IngredientState.Fresh, false) },
+                { 19, new(19, IngredientState.Fresh, false) },
+            };
+            List<ushort> input = [12, 6, 19, 2, 5, 3, 7];
+
+            // Act
+            var result = BuildFreshIngredientDatabase(input);
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(result, Has.Count.EqualTo(expected.Count));
+                Assert.That(result, Is.EquivalentTo(expected));
+            });
+        }
+
+        [Test]
+        public void BuildFreshIngredientDatabase_WithDuplicateMixedNumbers_ReturnsDictionaryWithoutDuplicates()
+        {
+            // Arrange
+            var expected = new Dictionary<ushort, Ingredient>
+            {
+                { 2, new(2, IngredientState.Fresh, false) },
+                { 3, new(3, IngredientState.Fresh, true) },
+                { 5, new(5, IngredientState.Fresh, false) },
+                { 6, new(6, IngredientState.Fresh, true) },
+                { 7, new(7, IngredientState.Fresh, false) },
+                { 12, new(12, IngredientState.Fresh, false) },
+                { 19, new(19, IngredientState.Fresh, false) },
+            };
+            List<ushort> input = [12, 6, 19, 2, 5, 3, 7, 6, 3];
+
+            // Act
+            var result = BuildFreshIngredientDatabase(input);
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(result, Has.Count.EqualTo(expected.Count));
+                Assert.That(result, Is.EquivalentTo(expected));
+            });
+        }
+
+        [Test]
+        public void GetFreshIngredientCountFromAvailableIngredientIds_NullIngredientIdsInput_ThrowsArgumentNullException()
+        {
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() => GetFreshIngredientCountFromAvailableIngredientIds(null!, []));
+        }
+
+        [Test]
+        public void GetFreshIngredientCountFromAvailableIngredientIds_NullFreshIngredientIdsInput_ThrowsArgumentNullException()
+        {
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() => GetFreshIngredientCountFromAvailableIngredientIds([], null!));
+        }
+
+        [Test]
+        public void GetFreshIngredientCountFromAvailableIngredientIds_NullAllInput_ThrowsArgumentNullException()
+        {
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() => GetFreshIngredientCountFromAvailableIngredientIds(null!, null!));
+        }
+
+        [Test]
+        public void GetFreshIngredientCountFromAvailableIngredientIds_EmptyIngredientIdsList_ReturnsZero()
+        {
+            // Arrange
+            List<ushort> ingredientIds = [];
+            Dictionary<ushort, Ingredient> ingredientDatabase = new()
+            {
+                { 2, new(2, IngredientState.Fresh, false) },
+                { 7, new(7, IngredientState.Fresh, true) },
+                { 12, new(12, IngredientState.Fresh, false) },
+                { 22, new(22, IngredientState.Fresh, false) },
+            };
+
+            // Act
+            var result = GetFreshIngredientCountFromAvailableIngredientIds(ingredientIds, ingredientDatabase);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(0u));
+        }
+
+        [Test]
+        public void GetFreshIngredientCountFromAvailableIngredientIds_EmptyIngredientDatabase_ReturnsZero()
+        {
+            // Arrange
+            List<ushort> ingredientIds = [2, 6, 8, 12, 16];
+            Dictionary<ushort, Ingredient> ingredientDatabase = [];
+
+            // Act
+            var result = GetFreshIngredientCountFromAvailableIngredientIds(ingredientIds, ingredientDatabase);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(0u));
+        }
+
+        [Test]
+        public void GetFreshIngredientCountFromAvailableIngredientIds_ValidInput_ReturnsCorrectResult()
+        {
+            // Arrange
+            List<ushort> ingredientIds = [2, 6, 8, 12, 16];
+            Dictionary<ushort, Ingredient> ingredientDatabase = new()
+            {
+                { 2, new(2, IngredientState.Fresh, false) },
+                { 7, new(7, IngredientState.Fresh, true) },
+                { 12, new(12, IngredientState.Fresh, false) },
+                { 22, new(22, IngredientState.Fresh, false) },
+            };
+
+            // Act
+            var result = GetFreshIngredientCountFromAvailableIngredientIds(ingredientIds, ingredientDatabase);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(2u));
         }
     }
 }
