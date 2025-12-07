2e3ebf4371e4498910acfd3629e0ce1bdc54a31c
Elliot Moule <ellimyt@live.com.au>
2025-12-07 17:41:20 +1100

    [Day 7 - Part 1 Complete]

---
+++ AdventOfCode.Business/Year2025/Day7.cs
@@ -0,0 +1,132 @@
+﻿namespace AdventOfCode.Business.Year2025
+{
+    internal class Day7 : IAdventDay
+    {
+        public void ExecutePart1()
+        {
+            var day7Input = File.ReadAllLines(Path.Combine("Year2025", "Resources", "Actual", "Day7_Input.txt"));
+            var result = CountSplits(day7Input);
+
+            Console.WriteLine($"\r\nThe beam will be split '{result}' times.\r\n");
+        }
+
+        public void ExecutePart2()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static int CountSplits(string[] grid)
+        {
+            ArgumentNullException.ThrowIfNull(grid);
+
+            if (grid.Length == 0)
+            {
+                return 0;
+            }
+
+            var startPos = FindStartPosition(grid);
+            return startPos == null
+                ? throw new ArgumentException("No starting position 'S' found in grid")
+                : SimulateBeams(grid, startPos.Value.Row, startPos.Value.Col);
+        }
+
+        internal static (int Row, int Col)? FindStartPosition(string[] grid)
+        {
+            for (int r = 0; r < grid.Length; r++)
+            {
+                for (int c = 0; c < grid[r].Length; c++)
+                {
+                    if (grid[r][c] == 'S')
+                    {
+                        return (r, c);
+                    }
+                }
+            }
+            return null;
+        }
+
+        internal static int SimulateBeams(string[] grid, int startRow, int startCol)
+        {
+            int splitCount = 0;
+            Queue<Beam> queue = new();
+            HashSet<(int, int, int, int)> visited = [];
+
+            queue.Enqueue(new Beam(startRow, startCol, 1, 0));
+
+            int iterations = 0;
+            while (queue.Count > 0)
+            {
+                iterations++;
+                var beam = queue.Dequeue();
+
+                var newBeams = ProcessBeam(grid, beam, visited, out bool didSplit);
+
+                if (didSplit)
+                {
+                    splitCount++;
+                }
+
+                foreach (var newBeam in newBeams)
+                {
+                    queue.Enqueue(newBeam);
+                }
+            }
+
+            return splitCount;
+        }
+
+        internal static List<Beam> ProcessBeam(string[] grid, Beam beam, HashSet<(int, int, int, int)> visited, out bool didSplit)
+        {
+            didSplit = false;
+            var newBeams = new List<Beam>();
+
+            int newRow = beam.Row + beam.DirRow;
+            int newCol = beam.Col + beam.DirCol;
+
+            if (!IsInBounds(grid, newRow, newCol))
+            {
+                return newBeams;
+            }
+
+            var state = (newRow, newCol, beam.DirRow, beam.DirCol);
+            if (visited.Contains(state))
+            {
+                return newBeams;
+            }
+
+            visited.Add(state);
+
+            char cell = grid[newRow][newCol];
+
+            if (cell == '^')
+            {
+                didSplit = true;
+                newBeams.AddRange(CreateSplitBeams(newRow, newCol));
+            }
+            else if (cell == '.' || cell == 'S')
+            {
+                newBeams.Add(new Beam(newRow, newCol, 1, 0));
+            }
+
+            return newBeams;
+        }
+
+        internal static bool IsInBounds(string[] grid, int row, int col)
+        {
+            return row >= 0 && row < grid.Length
+                && col >= 0 && col < grid[0].Length;
+        }
+
+        internal static List<Beam> CreateSplitBeams(int row, int col)
+        {
+            return
+            [
+                new(row, col, 1, -1), // Left
+                new(row, col, 1, 1)   // Right
+            ];
+        }
+
+        internal record Beam(int Row, int Col, int DirRow, int DirCol);
+    }
+}
+
---
+++ AdventOfCode.Test/Year2025/Day7Tests.cs
@@ -0,0 +1,293 @@
+﻿using static AdventOfCode.Business.Year2025.Day7;
+
+namespace AdventOfCode.Test.Year2025
+{
+    internal class Day7Tests
+    {
+        [Test]
+        public void CountCaretCharacters_NullInput_ThrowsArgumentNullException()
+        {
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() => CountSplits(null!));
+        }
+
+        [Test]
+        public void CountCaretCharacters_EmptyInput_ReturnsZero()
+        {
+            // Act
+            var result = CountSplits([]);
+
+            // Assert
+            Assert.That(result, Is.Zero);
+        }
+
+        readonly string[] _simpleGrid =
+        [
+            "...S...",
+            ".......",
+            "...^...",
+            "......."
+        ];
+
+        [Test]
+        public void FindStartPosition_WithValidGrid_ReturnsCorrectPosition()
+        {
+            // Act
+            var result = FindStartPosition(_simpleGrid);
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(result, Is.Not.Null);
+                Assert.That(result!.Value.Row, Is.EqualTo(0));
+                Assert.That(result.Value.Col, Is.EqualTo(3));
+            });
+        }
+
+        [Test]
+        public void FindStartPosition_WithExampleGrid_ReturnsCorrectPosition()
+        {
+            // Arrange
+            var input = File.ReadAllLines(Path.Combine("Year2025", "Resources", "Example", "Day7_Input.txt"));
+
+            // Act
+            var result = FindStartPosition(input);
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(result, Is.Not.Null);
+                Assert.That(result!.Value.Row, Is.EqualTo(0));
+                Assert.That(result.Value.Col, Is.EqualTo(7));
+            });
+        }
+
+        [Test]
+        public void FindStartPosition_WithNoStart_ReturnsNull()
+        {
+            // Arrange
+            var gridWithoutStart = new[] {
+            ".......",
+            "...^...",
+            "......."
+        };
+
+            // Act
+            var result = FindStartPosition(gridWithoutStart);
+
+            // Assert
+            Assert.That(result, Is.Null);
+        }
+
+        [Test]
+        public void IsInBounds_WithValidCoordinates_ReturnsTrue()
+        {
+            // Act
+            var result = IsInBounds(_simpleGrid, 1, 3);
+
+            // Assert
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void IsInBounds_WithNegativeRow_ReturnsFalse()
+        {
+            // Act
+            var result = IsInBounds(_simpleGrid, -1, 3);
+
+            // Assert
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void IsInBounds_WithNegativeColumn_ReturnsFalse()
+        {
+            // Act
+            var result = IsInBounds(_simpleGrid, 1, -1);
+
+            // Assert
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void IsInBounds_WithRowTooLarge_ReturnsFalse()
+        {
+            // Act
+            var result = IsInBounds(_simpleGrid, 10, 3);
+
+            // Assert
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void IsInBounds_WithColumnTooLarge_ReturnsFalse()
+        {
+            // Act
+            var result = IsInBounds(_simpleGrid, 1, 20);
+
+            // Assert
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void CreateSplitBeams_CreatesLeftAndRightBeams()
+        {
+            // Act
+            var beams = CreateSplitBeams(5, 10);
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(beams, Has.Count.EqualTo(2));
+
+                Assert.That(beams[0].Row, Is.EqualTo(5));
+                Assert.That(beams[0].Col, Is.EqualTo(10));
+                Assert.That(beams[0].DirRow, Is.EqualTo(1));
+                Assert.That(beams[0].DirCol, Is.EqualTo(-1));
+
+                Assert.That(beams[1].Row, Is.EqualTo(5));
+                Assert.That(beams[1].Col, Is.EqualTo(10));
+                Assert.That(beams[1].DirRow, Is.EqualTo(1));
+                Assert.That(beams[1].DirCol, Is.EqualTo(1));
+            });
+        }
+
+        [Test]
+        public void ProcessBeam_WithEmptySpace_ContinuesInSameDirection()
+        {
+            // Arrange
+            var beam = new Beam(0, 3, 1, 0);
+            var visited = new HashSet<(int, int, int, int)>();
+
+            // Act
+            var newBeams = ProcessBeam(_simpleGrid, beam, visited, out bool didSplit);
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(didSplit, Is.False);
+                Assert.That(newBeams, Has.Count.EqualTo(1));
+                Assert.That(newBeams[0].Row, Is.EqualTo(1));
+                Assert.That(newBeams[0].Col, Is.EqualTo(3));
+                Assert.That(newBeams[0].DirRow, Is.EqualTo(1));
+                Assert.That(newBeams[0].DirCol, Is.EqualTo(0));
+            });
+        }
+
+        [Test]
+        public void ProcessBeam_WithSplitter_CreatesTwoBeams()
+        {
+            // Arrange
+            var beam = new Beam(1, 3, 1, 0); // Moving down toward splitter
+            var visited = new HashSet<(int, int, int, int)>();
+
+            // Act
+            var newBeams = ProcessBeam(_simpleGrid, beam, visited, out bool didSplit);
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(didSplit, Is.True);
+                Assert.That(newBeams, Has.Count.EqualTo(2));
+            });
+        }
+
+        [Test]
+        public void ProcessBeam_OutOfBounds_ReturnsNoBeams()
+        {
+            // Arrange
+            var beam = new Beam(3, 3, 1, 0); // At bottom, moving down
+            var visited = new HashSet<(int, int, int, int)>();
+
+            // Act
+            var newBeams = ProcessBeam(_simpleGrid, beam, visited, out bool didSplit);
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(didSplit, Is.False);
+                Assert.That(newBeams, Is.Empty);
+            });
+        }
+
+        [Test]
+        public void ProcessBeam_AlreadyVisited_ReturnsNoBeams()
+        {
+            // Arrange
+            var beam = new Beam(0, 3, 1, 0);
+            var visited = new HashSet<(int, int, int, int)> { (1, 3, 1, 0) };
+
+            // Act
+            var newBeams = ProcessBeam(_simpleGrid, beam, visited, out bool didSplit);
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(didSplit, Is.False);
+                Assert.That(newBeams, Is.Empty);
+            });
+        }
+
+        [Test]
+        public void SimulateBeams_WithSimpleGrid_CountsOneSplit()
+        {
+            // Arrange & Act
+            var splitCount = SimulateBeams(_simpleGrid, 0, 3);
+
+            // Assert
+            Assert.That(splitCount, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void SimulateBeams_WithExampleGrid_Counts21Splits()
+        {
+            // Arrange
+            var input = File.ReadAllLines(Path.Combine("Year2025", "Resources", "Example", "Day7_Input.txt"));
+
+            // Act
+            var splitCount = SimulateBeams(input, 0, 7);
+
+            // Assert
+            Assert.That(splitCount, Is.EqualTo(21));
+        }
+
+        [Test]
+        public void CountSplits_WithSimpleGrid_CountsOneSplit()
+        {
+            // Arrange & Act
+            var splitCount = CountSplits(_simpleGrid);
+
+            // Assert
+            Assert.That(splitCount, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void CountSplits_WithExampleGrid_Counts21Splits()
+        {
+            // Arrange
+            var input = File.ReadAllLines(Path.Combine("Year2025", "Resources", "Example", "Day7_Input.txt"));
+
+            // Act
+            var splitCount = CountSplits(input);
+
+            // Assert
+            Assert.That(splitCount, Is.EqualTo(21));
+        }
+
+        [Test]
+        public void CountSplits_WithNoStartPosition_ThrowsException()
+        {
+            // Arrange
+            var gridWithoutStart = new[]
+            {
+                ".......",
+                "...^...",
+                "......."
+            };
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => CountSplits(gridWithoutStart));
+        }
+    }
+
+}
